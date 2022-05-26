a46a1de96a6330ecca2a630423c05c1c9c24815f
Mike Lynch <mlynch@sydney.edu.au>
2022-05-26 12:56:24 +1000

    requirements.txt, setup.cfg and stub xnat test

---
+++ tests/test_xnat.py
@@ -0,0 +1,25 @@
+from xnat4tests import launch_xnat, stop_xnat, connect, config
+
+
+def test_basic_xnat():
+    launch_xnat()
+
+    # Run your tests
+    with connect() as login:
+        PROJECT = "MYPROJECT"
+        SUBJECT = "MYSUBJECT"
+        SESSION = "MYSESSION"
+
+        login.put("/data/archive/projects/MY_TEST_PROJECT")
+
+        # Create subject
+        xsubject = login.classes.SubjectData(
+            label=SUBJECT, parent=login.projects[PROJECT]
+        )
+        # Create session
+        login.classes.MrSessionData(label=SESSION, parent=xsubject)
+
+    assert [p.name for p in (config.XNAT_ROOT_DIR / "archive").iterdir()] == [PROJECT]
+
+    # Remove the container after you are done (not strictly necessary)
+    stop_xnat()
