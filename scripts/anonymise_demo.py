2dad833675ecce6daae0ace516321fd9086cad05
Mike Lynch <m.lynch@sydney.edu.au>
2023-07-19 11:22:09 +1000

    Testing ability to crosswalk DICOM keywords to tags for anonymisation config

--- scripts/anonymise_demo.py
+++ scripts/anonymise_demo.py
@@ -4,13 +4,29 @@
 from dicomanonymizer import anonymize, keep
 from pydicom import dcmread
 
+from pydicom.tag import Tag
+
+
 INFILE = "./anonymizer/000000.dcm"
 OUTFILE = "./anonymizer/000000_anon.dcm"
 
 CUSTOM_RULES = {(0x0010, 0x0020): keep, (0x0010, 0x0010): keep}
 
+CUSTOM_RULES_KEYWORDS = {"StudyDate": keep, "AccessionNumber": keep}
 
-anonymize(INFILE, OUTFILE, CUSTOM_RULES, True)
+rules = {}
+
+for keyword, action in CUSTOM_RULES_KEYWORDS.items():
+    try:
+        tag = Tag(keyword)
+        rules[(tag.group, tag.elem)] = action
+    except ValueError as e:
+        print(f"{e} Unknown DICOM keyword {keyword}")
+
+print(rules)
+
+
+anonymize(INFILE, OUTFILE, rules, True)
 
 dc = dcmread(OUTFILE)
 
